37924dd322098de98bdc6d2107a52c46c2335cd8
EDM <EDM@DESKTOP-49047VP>
2021-10-19 19:30:46 -0500

    Agregar archivos de proyecto.

---
+++ AppS3/AppS3/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppS3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string usuario = txtUsuario.Text;
+                string clave = txtClave.Text;
+                //Permite abrir la nueva ventana
+                await Navigation.PushAsync(new PageDos(usuario, clave));
+
+            }
+            catch(Exception ex)
+            {
+               await DisplayAlert("Mensaje de alerta ", ex.Message, "OK");
+            }
+
+        }
+    }
+}
---
+++ AppS3/AppS3/PageDos.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageDos : ContentPage
+    {
+        public PageDos(string usuario, string clave)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+            lblClave.Text = clave;
+            txtUsuario.Text = usuario;
+            txtClave.Text = clave;
+        }
+    }
+}
